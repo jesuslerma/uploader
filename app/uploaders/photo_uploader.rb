92b9538f591f2234b8b96c9b82c12bb649d6b755
jesus lerma sanchez <shuyojl@gmail.com>
2016-04-26 20:51:27 -0500

    add images with carrierwave and thumb versions

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -28,16 +28,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  #process resize_to_fit: [400, 400]
+  process resize_to_fit: [400, 400]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  #version :thumb do
-    #process resize_to_fill: [100, 100]
-  #end
+  version :thumb do
+    process resize_to_fill: [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
